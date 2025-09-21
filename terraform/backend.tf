982dec62204504b4560a2bf76a781e8f1fda0c7e
Jona Koudijs <jona@koudijs.me>
2025-09-22 00:29:22 +0200

    Test secrets

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket                      = var.state_bucket_name
+    #bucket                      = var.state_bucket_name
     key                         = "platform/terraform.tfstate"
     region                      = "us-east-1"                           # required but not used by Hetzner
     endpoint                    = "https://fsn1.your-objectstorage.com" # change to your region endpoint (e.g., fsn1, nbx1, hel1)
@@ -8,7 +8,7 @@ terraform {
     skip_credentials_validation = true
     skip_region_validation      = true
     skip_metadata_api_check     = true
-    access_key                  = var.state_bucket_access_key
-    secret_key                  = var.state_bucket_secret_key
+    #access_key                  = var.state_bucket_access_key
+    #secret_key                  = var.state_bucket_secret_key
   }
 }
