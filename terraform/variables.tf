7ea30350e71cc8c3aa8a8b2f021e177bad280c38
Jona Koudijs <jona@koudijs.me>
2025-09-22 09:26:40 +0200

    Test secrets

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -2,14 +2,3 @@
 variable "hcloud_token" {
   sensitive = true
 }
-
-# Terraform state
-variable "state_bucket_name" {
-  sensitive = true
-}
-variable "state_bucket_access_key" {
-  sensitive = true
-}
-variable "state_bucket_secret_key" {
-  sensitive = true
-}
