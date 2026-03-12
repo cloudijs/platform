39cc1f9c36962daa72104ff4af369f71f745fe2e
Jona Koudijs <jona@koudijs.me>
2026-03-12 01:01:40 +0100

    Add ClusterIssuer to TF setup

--- infra/modules/cloudijs-system/main.tf
+++ infra/modules/cloudijs-system/main.tf
@@ -12,6 +12,14 @@ resource "kubernetes_namespace_v1" "platform_system" {
   }
 }
 
+# Configure Cert Manager
+resource "helm_release" "cert_manager" {
+  name       = "cert-manager-config"
+  namespace  = "cert-manager"
+
+  chart      = "${path.module}/charts/cert-manager"
+}
+
 # Deploy and configure Flux Operator
 resource "helm_release" "flux_operator" {
   depends_on = [kubernetes_namespace_v1.platform_system]
